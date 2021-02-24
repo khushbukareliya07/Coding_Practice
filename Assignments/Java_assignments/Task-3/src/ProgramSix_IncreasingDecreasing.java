bf4d78e4d5c9bc4bdc429db7033583a55019b127
khushbukareliya07 <khushbu.karelia@gmail.com>
2021-02-24 12:08:32 -0500

    program six completed.

--- Assignments/Java_assignments/Task-3/src/ProgramSix_IncreasingDecreasing.java
+++ Assignments/Java_assignments/Task-3/src/ProgramSix_IncreasingDecreasing.java
@@ -3,17 +3,40 @@ import java.util.Scanner;
 public class ProgramSix_IncreasingDecreasing {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        int n = obj.nextInt();
-        int i = 0;
-        char numberArr [] = String.valueOf(n).toCharArray();
+        System.out.println("Enter First Number");
+        int n1 = obj.nextInt();
+        System.out.println("Enter Second Number");
+        int n2 = obj.nextInt();
+        System.out.println("Enter Third Number");
+        int n3 = obj.nextInt();
 
-        for(int j =0; j < numberArr.length - 1; j++)
+        if(n1> n2)
         {
-            if(numberArr[j] > numberArr[j+1])
+            if(n2>n3)
             {
-                continue;
+                System.out.println("DECREASING");
             }
+            else
+            {
+                System.out.println("Neither increasing nor decreasing order.");
+            }
+
         }
+        else    //n1< n2
+        {
+            if(n2 < n3)
+            {
+                System.out.println("INCREASING");
+            }
+            else
+            {
+                System.out.println("Neither increasing nor decreasing order.");
+            }
+
+        }
+
+
+
 
 
     }
